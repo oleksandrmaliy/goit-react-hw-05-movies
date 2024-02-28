b9da528d71ca0797778db5d182bceb9f5fe00b82
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-02-28 16:32:31 +0200

    color

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#07853e'
       }}
     >
       React homework template hw 5
